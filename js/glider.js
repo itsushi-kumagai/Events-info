ed4ba9784ef5bfd3c0ab59c7b50f3d69e72e6bd6
ikumagai <ittsu.2060@ezweb.ne.jp>
2019-09-19 12:39:33 +0900

    second commit

---
+++ js/glider.js
@@ -0,0 +1,27 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 3,
+    slidesToScroll: 10,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+});
+
+new Glider(document.querySelector('.glider2'), {
+    slidesToShow: 3,
+    slidesToScroll: 10,
+    draggable: true,
+    dots: '#dots2',
+    arrows: {
+        prev: '.glider-prev2',
+        next: '.glider-next2'
+    }
+});
+
+
+
+
+
+  
